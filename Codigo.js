575035e45220fbcda2675e4306ce9db04bf2b0e1
JoiiP29 <126031269+JoiiP29@users.noreply.github.com>
2023-02-26 12:14:13 -0300

    Add files via upload

---
+++ Codigo.js
@@ -0,0 +1,10 @@
+document.getElementById('boton').onclick= function (){
+    console.log ("captura del evento click");
+    document.getElementById ("demo").innerHTML= "Enviado exitosamente!";
+}
+
+
+
+
+
+
